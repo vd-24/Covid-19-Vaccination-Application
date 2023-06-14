a12611ae1f10c2169deba947921eba8bc9764a32
Ankitfozzy <ankitsinghsisodia610@gmail.com>
2023-06-14 17:49:55 +0530

    day2

--- covid-19/src/main/java/com/covid19/Models/Vaccine.java
+++ covid-19/src/main/java/com/covid19/Models/Vaccine.java
@@ -13,4 +13,42 @@ public class Vaccine {
 	private Integer vaccineId;
 	private String vaccineName;
 	private String description;
+	
+	public Vaccine() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Vaccine(String vaccineName, String description) {
+		super();
+		this.vaccineName = vaccineName;
+		this.description = description;
+	}
+
+	public Integer getVaccineId() {
+		return vaccineId;
+	}
+
+	public void setVaccineId(Integer vaccineId) {
+		this.vaccineId = vaccineId;
+	}
+
+	public String getVaccineName() {
+		return vaccineName;
+	}
+
+	public void setVaccineName(String vaccineName) {
+		this.vaccineName = vaccineName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
 }
