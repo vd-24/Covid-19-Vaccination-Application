1e8a658374134475149f7afd76249eaf8a21ba1e
Rajat24111994 <rajatmohangpt3@gmail.com>
2023-06-15 00:07:24 +0530

    day3

---
+++ covid-19/src/main/java/com/covid19/Exception/VaccinationCenterException.java
@@ -0,0 +1,13 @@
+package com.covid19.Exception;
+
+public class VaccinationCenterException extends Exception {
+
+	public VaccinationCenterException() {
+		
+	}
+	
+	public VaccinationCenterException(String msg) {
+		
+		super(msg);
+	}
+}
