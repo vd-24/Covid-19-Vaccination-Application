2f3d805524add86ca40e085ae4a78b0dcf9ff95e
vd-24 <deshmukhjeet6@gmail.com>
2023-06-17 18:33:59 +0530

    a

--- covid-19/src/main/java/com/covid19/Exception/VaccineRegistrationException.java
+++ covid-19/src/main/java/com/covid19/Exception/VaccineRegistrationException.java
@@ -1,15 +1,8 @@
 package com.covid19.Exception;
 
 public class VaccineRegistrationException extends RuntimeException{
-<<<<<<< HEAD
 	public VaccineRegistrationException(String msg) {
 		super(msg);
 	}
-=======
 
-	public VaccineRegistrationException(String message) {
-		super(message);
-	}
-	
->>>>>>> main
 }
